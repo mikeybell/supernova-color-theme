753ed8b4262d6f374a1a5b5426aad6821fd423c1
mikeybell <michaelrobertbell@gmail.com>
2019-02-18 10:26:48 -0500

    First pass at token colors

--- constants.js
+++ constants.js
@@ -7,11 +7,18 @@ const text2 = "#5F7E97"
 const text3 = "#384B5C"
 
 const darkBlue = "#084D81";
+const medBlue = "#6E9CFF";
 const lightBlue = "#89DDFF";
 const teal = "#80CBC4";
 const green = "#A5EB56";
 const yellow = "#FFCA28";
 const purple = "#7B44FD";
-const pink = "#DE4383";
 const red = "#EE4845";
+
+const purpleText = "#C792EA"
+const pinkText = "#FC60A1"
+const redText = "#F07178"
+const greenText = "#C3E88D"
+const yellowText = "#FFDC6B"
+const orangeText = "#FA854E"
 
