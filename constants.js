4dd1cfd002288ca6075839a1489cc25380e4574a
mikeybell <michaelrobertbell@gmail.com>
2019-02-17 22:24:41 -0500

    First pass at ui colors

---
+++ constants.js
@@ -0,0 +1,17 @@
+const background = "#212834";
+const backgroundDark = "#1C222D";
+const backgroundDarkest = "#141820";
+
+const text1 = "#D6DEEB"
+const text2 = "#5F7E97"
+const text3 = "#384B5C"
+
+const darkBlue = "#084D81";
+const lightBlue = "#89DDFF";
+const teal = "#80CBC4";
+const green = "#A5EB56";
+const yellow = "#FFCA28";
+const purple = "#7B44FD";
+const pink = "#DE4383";
+const red = "#EE4845";
+
