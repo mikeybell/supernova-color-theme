f14fbf29063cde289faadfd2b9f23b07f8dc5dd3
Mike Bell <michaelrobertbell@gmail.com>
2019-02-18 17:37:58 -0500

    Color tweaks

--- constants.js
+++ constants.js
@@ -5,10 +5,12 @@ const backgroundDarkest = "#141820";
 const text1 = "#D6DEEB"
 const text2 = "#7999B3"
 const text3 = "#384B5C"
+const comment = "#416480"
 
 const darkBlue = "#084D81";
 const medBlue = "#6E9CFF";
 const lightBlue = "#89DDFF";
+const lightestBlue = "#B9E5FF";
 const green = "#C3E88D";
 const yellow = "#FFCA28";
 const purple = "#7B44FD";
