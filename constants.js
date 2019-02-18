434de59b95dd02cc6e3a8c5c7b87e18009cc23a6
mikeybell <michaelrobertbell@gmail.com>
2019-02-18 10:47:53 -0500

    Cleans up colors

--- constants.js
+++ constants.js
@@ -9,8 +9,7 @@ const text3 = "#384B5C"
 const darkBlue = "#084D81";
 const medBlue = "#6E9CFF";
 const lightBlue = "#89DDFF";
-const teal = "#80CBC4";
-const green = "#A5EB56";
+const green = "#C3E88D";
 const yellow = "#FFCA28";
 const purple = "#7B44FD";
 const red = "#EE4845";
@@ -18,7 +17,6 @@ const red = "#EE4845";
 const purpleText = "#C792EA"
 const pinkText = "#FC60A1"
 const redText = "#F07178"
-const greenText = "#C3E88D"
 const yellowText = "#FFDC6B"
 const orangeText = "#FA854E"
 
