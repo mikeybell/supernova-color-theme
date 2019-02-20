9285a0c41b02c35c014051c163171af13c54543f
Mike Bell <michaelrobertbell@gmail.com>
2019-02-19 21:24:54 -0500

    Color tweaks

---
+++ colors.js
@@ -0,0 +1,23 @@
+// UI Colors
+const background =        "#212834";
+const backgroundDark =    "#1C222D";
+const backgroundDarkest = "#141820";
+
+const uiBlue =            "#084D81";
+const uiPurple =          "#8D71FD";
+
+// Text Colors
+const text1 =             "#D6DEEB"
+const text2 =             "#7999B3"
+const text3 =             "#384B5C"
+const comment =           "#416480"
+
+const purple =            "#C792EA"
+const blue =              "#82AAFF";
+const lightBlue =         "#89DDFF";
+const lightestBlue =      "#B9E5FF";
+const green =             "#C3E88D";
+const yellow =            "#F1DC3D";
+const orange =            "#FA854E"
+const red =               "#E97178"
+const pink =              "#FC60A1"
